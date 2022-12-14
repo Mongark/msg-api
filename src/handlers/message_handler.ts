fcfeaf47307c0148d9857da3f547fbd824bd8f6e
Mongark <mongarkprogrammer@gmail.com>
2022-12-14 15:52:07 -0400

    Fixed test for 'POST /message'

--- src/handlers/message_handler.ts
+++ src/handlers/message_handler.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
-function post_message(req: Request, res: Response) {
-
+function post_message(req: Request, res: Response, next: NextFunction) {
+    next();
 }
 
 const message_handler = { post_message };
--- tests/message.test.ts
+++ tests/message.test.ts
@@ -5,7 +5,10 @@ import app from "../src/app";
 describe("Routes in 'message_router'", () => {
     const server = supertest(app);
 
-    it("should post an object successfully", () => {
+    it("should post an object successfully", async() => {
+        const res = await server
+            .post("/message")
+            .send({msg: "Message"});
 
     });
 });
