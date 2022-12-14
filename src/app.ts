8dea550324b85715d264c7bb239c95d5bb0e2f2e
Mongark <mongarkprogrammer@gmail.com>
2022-12-14 16:54:32 -0400

    Added configuration for Mongoose

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,19 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import message_router from "./routes/message_router";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
 const healthRouter = express.Router();
 
-dotenv.config();
+const MONGO_URL: string = (!process.env.DATABASE_URL) ? "" : process.env.DATABASE_URL;
+
+mongoose.connect(MONGO_URL);
+const database = mongoose.connection;
+
+database.on('error', (err) => console.log(err));
 
 app.use(express.json());
 app.use(cors());
