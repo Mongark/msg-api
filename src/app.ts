0cdae69de58d65a60398bf2df00731e084fd6a99
Mongark <mongarkprogrammer@gmail.com>
2022-12-14 15:11:11 -0400

    Removed package 'body-parser'

--- src/app.ts
+++ src/app.ts
@@ -5,15 +5,13 @@ import bodyParser from "body-parser";
 const app = express();
 const healthRouter = express.Router();
 
-app.use(bodyParser.text());
-// app.use(bodyParser.urlencoded());
-// app.use(cors());
+app.use(express.json());
+app.use(cors());
 
 app.get('/ping', (req, res) => {
     res.setHeader("content-type", "application/text");
     res.status(200);
-    res.send("pong")
-    res.end();
+    res.send("pong");
 });
 
 app.use("/", healthRouter);
