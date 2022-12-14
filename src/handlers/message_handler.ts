d0988131c292c2af74b8946749024ef6caad941c
Mongark <mongarkprogrammer@gmail.com>
2022-12-14 15:39:59 -0400

    Implemented route for 'POST /message'

--- src/handlers/message_handler.ts
+++ src/handlers/message_handler.ts
@@ -1,1 +1,9 @@
-export {};
+import { Request, Response } from "express";
+
+function post_message(req: Request, res: Response) {
+
+}
+
+const message_handler = { post_message };
+
+export default  message_handler;
--- src/routes/message_router.ts
+++ src/routes/message_router.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
 
+import message_handler from "../handlers/message_handler";
+
 const message_router = Router();
+
+message_router.use(message_handler.post_message);
 
 export default message_router;
