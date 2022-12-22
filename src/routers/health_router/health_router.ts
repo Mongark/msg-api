aa206cb6b6dd78e2160eb166994ad7f14ff56a99
Mongark <mongarkprogrammer@gmail.com>
2022-12-22 14:50:47 -0400

    Bug fixing and added dev script  - Fixed issue with 'GET /ping' endpoint

--- src/routers/health_router/health_router.ts
+++ src/routers/health_router/health_router.ts
@@ -1,7 +1,9 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 
 const health_router = Router();
 
-health_router.get('/ping', (req: Request, res: Response) => res.status(200).json("pong"));
+health_router.get('/ping', (req: Request, res: Response) => {
+    res.status(200).json("pong");
+});
 
 export default health_router;
